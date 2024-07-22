717b71e36802fbab530fdb439c82b160889680fb
Asmitaa03 <asmitas32002@gmail.com>
2024-07-22 15:31:48 +0530

    Addition of two nos. with diff data type

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args){
+        int a = 5;
+        float b = 7.5f;
+        float sum;
+
+        sum = (float)a+b;
+        System.out.println("The sum is: " + sum);
+
+    }
+    
+}
