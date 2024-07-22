fbb048d9cde66d1ce4a7d008e7cdf772643578a6
Asmitaa03 <asmitas32002@gmail.com>
2024-07-22 16:33:19 +0530

    Ternary Operator

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator{
+    public static void main(String[] args){
+        int age = 21;
+       String adult= (age>=18)?"Elligible for voting":"Not elligible for voting";
+       System.out.println(adult);
+    }
+}
